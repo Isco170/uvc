d4f50bc4a8b4065433343dff21db9dcad1bb685a
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2025-11-18 12:53:55 +0200

    Adding a Heal Funcionality

--- section 04/prj-monster-01-starting-setup/app.js
+++ section 04/prj-monster-01-starting-setup/app.js
@@ -17,7 +17,7 @@ const app = Vue.createApp({
         playerBarStyles() {
             return { width: this.playerHealth + '%' }
         },
-        mayUseSpecialAttack(){
+        mayUseSpecialAttack() {
             return this.currentRound % 3 !== 0;
         }
     },
@@ -32,10 +32,20 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
         },
-        specialAttackMonster(){
+        specialAttackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20)
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100
+            } else {
+                this.playerHealth += healValue;
+            }
             this.attackPlayer();
         }
     }
