bd0d33d553e7d2c234e115e31058e46089bb0c3d
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2025-11-18 12:39:18 +0200

    Updating the Health Bars

--- section 04/prj-monster-01-starting-setup/app.js
+++ section 04/prj-monster-01-starting-setup/app.js
@@ -1,4 +1,4 @@
-function getRandomValue(min, max){
+function getRandomValue(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
@@ -8,6 +8,14 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100
         };
+    },
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' }
+        },
+        playerBarStyles() {
+            return { width: this.playerHealth + '%' }
+        }
     },
     methods: {
         attackMonster() {
